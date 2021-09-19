b1488e089bca0b0445b2c5b66920aa6ca15bb1da
Adrien Calmesnil <adcalmesnil@gmail.com>
2021-09-19 19:01:08 +0200

    last push

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var burger = document.getElementById("icon");
 // menu mobile display
 burger.onclick = function () {
   this.classList.toggle("change");
-  var menu = document.getElementById("menuMobile");
+  var menu = document.getElementById("menu_mobile");
 
   if (menu.style.display === "block") {
     menu.style.display = "none";
